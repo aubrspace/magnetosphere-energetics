5150f7db5e8e1619de02407bda861e542ef4965b
Austin Michael Brenner <aubr@umich.edu>
2020-05-07 13:30:47 -0400

    Linux specific batch file written in bash with eval command to set correct tecplot environment prior to executing scripts

---
+++ BATCH.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Batch file for running extract_mpsurface.py on all .plt files
+
+#create output folders
+PLTDIR=plt/
+SCRIPTDIR=./
+mkdir output
+mkdir output/plt
+mkdir output/lay
+mkdir output/png
+PLTOUT=output/plt
+LAYOUT=output/lay
+PNGOUT=output/png
+echo "Created output directory"
+
+#ensure batch mode is ready for tecpot LINUX SPECIFIC
+TECPATH=$(which tec360)
+eval `$TECPATH-env`
+echo LD_LIBRARY_PATH: $LD_LIBRARY_PATH
+
+#execute script on .plt files
+for file in $PLTDIR*.plt
+do
+    python extract_mpsurface.py $file $PLTOUT $LAYOUT $PNGOUT
+done
