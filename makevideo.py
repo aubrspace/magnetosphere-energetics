654589f2500b83b0fafe4f539e686d1a97c45c8a
Austin Brenner <ngpdl@Austins-MacBook-Pro.local>
2020-05-15 14:22:12 -0400

    updated makevideo script to take timeformatted files

--- makevideo.py
+++ makevideo.py
@@ -4,6 +4,24 @@
 
 import glob
 import os
+import sys
+import numpy as np
+
+def get_time(filename):
+    """Function gets time in seconds from file name assuming naming
+        convention: YEARMODAY-HRMINSECdone.png
+    Input
+        filename
+    Output
+        abstime
+    """
+    day = int(filename.split('/')[-1].split('-')[0])
+    time = int(filename.split('/')[-1].split('-')[1].split('d')[0])
+    hour = np.floor(time/10000)
+    minute = np.mod(time/100, 100)
+    second = np.mod(time, 100)
+    abstime = second + 60*(minute+ 60*(hour + 24*(day)))
+    return abstime
 
 def convert_pdf(folder, res):
     """Function converts .pdf files to .png files for compiling into video.
@@ -14,11 +32,32 @@ def convert_pdf(folder, res):
         res- horizontal resolution for .png file
     """
     for image in glob.glob(folder+'/*.pdf'):
-        frame = image.split('-')[1].split('.')[0]
+        frame = image.split('-')[1].split('d')[0]
         filename = './img-'+frame+'.png'
         convert_cmd = 'convert -density '+str(res)+' '+ image +' '+ filename
         os.system(convert_cmd)
         print(filename, 'has been converted')
+
+def set_frames(folder):
+    """function preps files with date time format see get_time
+    Input
+        folder
+    Output
+        framedir
+    """
+    #create sorted list of image files
+    framelist = sorted(glob.glob(folder+'/*.png'), key=get_time)
+    os.system('mkdir '+folder+'/frames/')
+    n=0
+    for image in framelist:
+        if n<0.9:
+            filename = 'img-0{:.0f}.png'.format(10*n)
+        else:
+            filename = 'img-{:.0f}.png'.format(10*n)
+        cp_cmd = 'cp '+image+' '+folder+'/frames/'+filename
+        os.system(cp_cmd)
+        n = n+0.1
+
 
 def vid_compile(folder, framerate, title):
     """function that compiles video from sequence of .png files
@@ -38,8 +77,10 @@ if __name__ == '__main__':
     #Video settings
     RES = 400
     FRAMERATE = 2
-    FOLDER = 'slice_log'
+    FOLDER = sys.argv[1]
+    #determine file frame order
+    set_frames(FOLDER)
     #Convert all img .pdf to img.png
-    convert_pdf(FOLDER, RES)
+    #convert_pdf(FOLDER, RES)
     #Create video from .png
-    vid_compile(FOLDER, FRAMERATE, 'video')
+    vid_compile(FOLDER+'/frames', FRAMERATE, 'video')
