8d68fcc4237a8d83f6a0abd243911a63b1be08f6
Austin Brenner <ngpdl@austins-mbp.lan>
2020-11-19 12:28:01 -0500

    two new files to process .idl ionoshpere data. Only useful for current run, future outputs from SWMF should be in tecplot to start

---
+++ proc_iono_idl.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Batch file for running extract_mpsurface.py on all .plt files
+
+#set path to .idl files and script
+IDLDIR=./ionosphere_data/
+SCRIPTDIR=./
+
+#execute script on .idl files
+for file in $IDLDIR*.idl
+do
+    python $SCRIPTDIR/idl2pandas.py $file $IDLDIR 
+done
