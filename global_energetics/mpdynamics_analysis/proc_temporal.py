e712aeea373a4011f655cd1bb1eab4bd7a17279d
Austin <aubr@umich.edu>
2021-02-03 22:14:32 -0500

    made input more general and flexible output path. Fixed for case of integral log only having one entry

--- global_energetics/mpdynamics_analysis/proc_temporal.py
+++ global_energetics/mpdynamics_analysis/proc_temporal.py
@@ -24,7 +24,8 @@ import tecplot as tp
 from tecplot.constant import *
 from tecplot.exception import *
 
-def plot_all_runs_1Qty(axis, dflist, dflabels, timekey, qtkey, ylabel):
+def plot_all_runs_1Qty(axis, dflist, dflabels, timekey, qtkey, ylabel, *,
+                       xlim=None, ylim=None):
     """Function plots cumulative energy over time on axis with lables
     Inputs
         axis- object plotted on
@@ -33,6 +34,9 @@ def plot_all_runs_1Qty(axis, dflist, dflabels, timekey, qtkey, ylabel):
         timekey, qtkey- used to located column with time and the qt to plot
     """
     for data in enumerate(dflist):
+        axis.plot(data[1][timekey],data[1][qtkey],
+                  label=dflabels[data[0]])
+        '''
         if dflabels[data[0]].find('flow') != -1:
             axis.plot(data[1][timekey],data[1][qtkey],color='orange',
                       label='Flowline')
@@ -45,6 +49,11 @@ def plot_all_runs_1Qty(axis, dflist, dflabels, timekey, qtkey, ylabel):
         if dflabels[data[0]].find('hybrid') != -1:
             axis.plot(data[1][timekey],data[1][qtkey],color='green',
                       label='Hybrid')
+        '''
+    if xlim!=None:
+        axis.set_xlim(xlim)
+    if ylim!=None:
+        axis.set_ylim(ylim)
     axis.set_xlabel(timekey)
     axis.set_ylabel(ylabel)
     axis.legend()
@@ -60,21 +69,22 @@ def get_energy_dataframes(dflist, dfnames):
     use_dflist, use_dflabels = [], []
     for df in enumerate(dflist):
         if not df[1].empty & (dfnames[df[0]].find('stat')==-1):
-            ###Add cumulative energy terms
-            #Compute cumulative energy In, Out, and Net
-            delta_t = (df[1]['Time [UTC]'].loc[1]-
-                       df[1]['Time [UTC]'].loc[0]).seconds
-            #use pandas cumulative sum method
-            cumu_E_in = df[1]['mp K_in [kW]'].cumsum()*delta_t
-            cumu_E_out = df[1]['mp K_out [kW]'].cumsum()*delta_t
-            cumu_E_net = df[1]['mp K_net [kW]'].cumsum()*delta_t
-            #Add column to dataframe
-            dflist[df[0]].loc[:,'CumulE_in [kJ]'] = cumu_E_in
-            dflist[df[0]].loc[:,'CumulE_out [kJ]'] = cumu_E_out
-            dflist[df[0]].loc[:,'CumulE_net [kJ]'] = cumu_E_net
-            ###Add modified dataframe to list
-            use_dflist.append(dflist[df[0]])
-            use_dflabels.append(dfnames[df[0]])
+            if len(df[1]) > 1:
+                ###Add cumulative energy terms
+                #Compute cumulative energy In, Out, and Net
+                delta_t = (df[1]['Time [UTC]'].loc[1]-
+                        df[1]['Time [UTC]'].loc[0]).seconds
+                #use pandas cumulative sum method
+                cumu_E_in = df[1]['mp K_in [kW]'].cumsum()*delta_t
+                cumu_E_out = df[1]['mp K_out [kW]'].cumsum()*delta_t
+                cumu_E_net = df[1]['mp K_net [kW]'].cumsum()*delta_t
+                #Add column to dataframe
+                dflist[df[0]].loc[:,'CumulE_in [kJ]'] = cumu_E_in
+                dflist[df[0]].loc[:,'CumulE_out [kJ]'] = cumu_E_out
+                dflist[df[0]].loc[:,'CumulE_net [kJ]'] = cumu_E_net
+                ###Add modified dataframe to list
+                use_dflist.append(dflist[df[0]])
+                use_dflabels.append(dfnames[df[0]])
     return use_dflist, use_dflabels
 
 def prepare_figures(dflist, dfnames, outpath):
@@ -94,18 +104,31 @@ def prepare_figures(dflist, dfnames, outpath):
         #Cumulative Energy, and Power
         if True:
             figname = 'PowerEnergy'
-            cumulative_E, (ax1,ax2) = plt.subplots(nrows=2,ncols=1,
+            cumulative_E, (ax1,ax2,ax3) = plt.subplots(nrows=3,ncols=1,
                                                sharex=True, figsize=[18,6])
             timekey = 'Time [UTC]'
-            qtykey = 'CumulE_net [kJ]'
-            ylabel = 'Cumulative Energy [kJ]'
+            qtykey = 'CumulE_in [kJ]'
+            ylabel = 'Cumulative Energy in [kJ]'
             plot_all_runs_1Qty(ax1, energy_dfs, energy_dfnames, timekey,
                                qtykey, ylabel)
-            qtykey = 'mp K_net [kW]'
-            ylabel = 'Net Power [kW]'
+            timekey = 'Time [UTC]'
+            qtykey = 'CumulE_out [kJ]'
+            ylabel = 'Cumulative Energy out [kJ]'
             plot_all_runs_1Qty(ax2, energy_dfs, energy_dfnames, timekey,
                                qtykey, ylabel)
+            timekey = 'Time [UTC]'
+            qtykey = 'CumulE_net [kJ]'
+            ylabel = 'Cumulative Energy net [kJ]'
+            plot_all_runs_1Qty(ax3, energy_dfs, energy_dfnames, timekey,
+                               qtykey, ylabel)
+            '''
+            qtykey = 'mp K_net [kW]'
+            ylabel = 'Power net [kW]'
+            plot_all_runs_1Qty(ax2, energy_dfs, energy_dfnames, timekey,
+                               qtykey, ylabel)
+            '''
             cumulative_E.savefig(outpath+'{}.png'.format(figname))
+            plt.show()
             plt.close(cumulative_E)
         ###################################################################
         #Volume and Surface Area
@@ -134,20 +157,19 @@ def prepare_figures(dflist, dfnames, outpath):
             qtykey = 'mp uB [J]'
             ylabel = 'Magnetic Energy [J]'
             plot_all_runs_1Qty(ax1, energy_dfs, energy_dfnames, timekey,
-                               qtykey, ylabel)
+                               qtykey, ylabel, ylim=[0,5e15])
             qtykey = 'mp KEpar [J]'
             ylabel = 'Parallel Kinetic Energy [J]'
             plot_all_runs_1Qty(ax2, energy_dfs, energy_dfnames, timekey,
-                               qtykey, ylabel)
+                               qtykey, ylabel, ylim=[0,3e15])
             qtykey = 'mp KEperp [J]'
             ylabel = 'Perpendicular Kinetic Energy [J]'
             plot_all_runs_1Qty(ax3, energy_dfs, energy_dfnames, timekey,
-                               qtykey, ylabel)
+                               qtykey, ylabel, ylim=[0,3e15])
             qtykey = 'mp Etherm [J]'
             ylabel = 'Thermal Energy [J]'
             plot_all_runs_1Qty(ax4, energy_dfs, energy_dfnames, timekey,
-                               qtykey, ylabel)
-            plt.show()
+                               qtykey, ylabel, ylim=[0,3e13])
             Volume_E.savefig(outpath+'{}.png'.format(figname))
             plt.close(Volume_E)
         ###################################################################
@@ -155,21 +177,20 @@ def prepare_figures(dflist, dfnames, outpath):
         print('Unable to create ___ plot, data missing!')
     pass
 
-def process_temporal_mp(*, data_path_integrated=None,data_path_stats=None,
-                           outputpath=None):
+def process_temporal_mp(data_path_list, outputpath):
     """Top level function handles time varying magnetopause data and
         generates figures according to settings set by inputs
     Inputs
-        data_path_integrated/stats- path to the data, default will skip
-        make_fig- bool for figures
+        data_path_list- paths to the data, default will skip
+        outputpaht
     """
-    if data_path_integrated == None and data_path_stats == None:
+    if data_path_list == []:
         print('Nothing to do, no data_paths were given!')
     else:
         approved = ['stats', 'shue', 'shue98', 'shue97', 'flow', 'hybrid', 'field']
         dflist, dfnames = [], []
         spin = Spinner('finding available temporal data ')
-        for path in [data_path_integrated, data_path_stats]:
+        for path in data_path_list:
             if path != None:
                 for datafile in glob.glob(path+'/*.h5'):
                     with pd.HDFStore(datafile) as hdf_file:
@@ -185,11 +206,11 @@ def process_temporal_mp(*, data_path_integrated=None,data_path_stats=None,
         prepare_figures(dflist, dfnames, outputpath)
 
 if __name__ == "__main__":
-    #IPATH = ('/Users/ngpdl/Code/swmf-energetics/output/'+
-    #                 'mpdynamics/jan27_3surf')
-    IPATH = ('/home/aubr/Code/swmf-energetics/output')
-    SPATH = ('/home/aubr/Code/swmf-energetics/output/meshdata/stats')
-    OPATH = ('/home/aubr/Code/swmf-energetics/output/figures/')
-    process_temporal_mp(data_path_integrated=IPATH,
-                        data_path_stats=SPATH,
-                        outputpath=OPATH)
+    PATH1 = ('/Users/ngpdl/Code/swmf-energetics/output/'+
+                     'mpdynamics/feb3')
+    PATH2 = ('/Users/ngpdl/Code/swmf-energetics/output/'+
+                     'mpdynamics/jan27_3surf')
+    #IPATH = ('/home/aubr/Code/swmf-energetics/output')
+    #SPATH = ('/home/aubr/Code/swmf-energetics/output/meshdata/stats')
+    #OPATH = ('/home/aubr/Code/swmf-energetics/output/figures/')
+    process_temporal_mp([PATH1,PATH2],PATH1+'/')
