502db0fa00c99cdfe80c5f9f086e621c93e7654a
Austin <aubr@umich.edu>
2021-02-17 14:40:57 -0500

    swmf_access file for dealing with things expected to be swmf specific

---
+++ global_energetics/extract/swmf_access.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Functions for handling data from swmf only
+"""
+import datetime as dt
+import tecplot as tp
+from tecplot.constant import *
+from tecplot.exception import *
+
+def swmf_read_time(*, zoneindex=0, key='TIMEEVENT' ):
+    """Function looks in auxillary data for time information
+    Inputs
+        zoneindex- 0 based index for which swmf field data is found
+        key- typically is TIMEEVENT, can update here
+    Outputs
+        datetime object of the event time
+    """
+    #make dict of aux data
+    aux_data = tp.active_frame().dataset.zone(zoneindex).aux_data
+    timestring = aux_data[key]
+    year = int(timestring.split('/')[0])
+    month = int(timestring.split('/')[1])
+    day = int(timestring.split('/')[2].split(' ')[0])
+    hour = int(timestring.split('/')[2].split(' ')[-1].split(':')[0])
+    minute = int(timestring.split('/')[2].split(' ')[-1].split(':')[1])
+    second = int(
+       timestring.split('/')[2].split(' ')[-1].split(':')[2].split('.')[0])
+    return dt.datetime(year,month,day,hour,minute,second)
+
