f9ddd13e093b7d5357f85547c119754837fcfaca
Austin Brenner <aubr@umich.edu>
2023-09-28 16:17:57 -0400

    refined 1/16th test has same result

--- glbatch-jobs.bash
+++ glbatch-jobs.bash
@@ -14,11 +14,14 @@
 #INPUTDIR=./ideal_test/GM/IO2/
 #OUTPUTDIR=./ideal_test_sp10-3/
 
-INPUTDIR=./ideal_IE1/GM/IO2/
-OUTPUTDIR=./ideal_IE1_sp10-3/
+#INPUTDIR=./ideal_IE1/GM/IO2/
+#OUTPUTDIR=./ideal_IE1_sp10-3/
 
 #INPUTDIR=./ideal_noRCM1/GM/IO2/
 #OUTPUTDIR=./ideal_noRCM1_sp10-3/
+
+INPUTDIR=./ideal_refined/GM/IO2/
+OUTPUTDIR=./ideal_refined_sp10-3/
 
 filecount=0
 workercount=0
