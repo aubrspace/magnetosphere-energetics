a05fe58a69ddbafea5eaef31432642f49eac786b
Austin Brenner <ambrenne@MacBookPro-128154174111.ndc.nasa.gov>
2025-06-16 14:38:55 -0400

    first pass at reading RBE output -> .npz binary

--- global_energetics/extract/radbelt.py
+++ global_energetics/extract/radbelt.py
@@ -10,10 +10,14 @@ from numpy import (sin,cos,deg2rad,pi)
 import datetime as dt
 from matplotlib import pyplot as plt
 from geopack import geopack as gp
+from tqdm import tqdm
+##
+from global_energetics.makevideo import time_sort
 
 def read_flux(infile:str,**kwargs:dict) -> dict:
     flux_dict = {}
-    print(f"\t\treading file")
+    if kwargs.get('verbose',False):
+        print(f"\t\treading file")
     with open(infile,'r') as f:
         # Grid
         gridline = f.readline()
@@ -22,10 +26,11 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
         ntimes = 0
         nLat,nMLT,nE,nAlpha = [int(n) for n in [nLat,nMLT,nE,nAlpha]]
         #NOTE the order of which grid appears first can differ
-        if any([nLat==nE,nE==nAlpha,nLat==nAlpha]):
-            if kwargs.get('safe',True):
+        if any([nLat==nE,nE==nAlpha,nLat==nAlpha]) and kwargs.get(
+                                                             'verbose',False):
+            if kwargs.get('safe',False):
                 print('ERROR!! Uncertain grid due to matching sizes ...')
-                exit
+                return
             else:
                 print('WARNING!! Uncertain grid due to matching sizes ...')
         grid_start = f.tell()
@@ -33,14 +38,15 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
         done = False
         while not done:
             line = f.readline()
-            if '!' in line:
+            if 'parmod' in line:
                 done = True
             else:
                 total_ngrid += len(line.split())
-        if total_ngrid - (nLat+nMLT+nE+nAlpha)>0: #MLT grid inferred
+        if total_ngrid - (nLat+nMLT+nE+nAlpha)<0: #MLT grid inferred
             # MLT bins
-            print(f'\t\t\t{nMLT} MLTs')
-            mlt_lvls = list(np.linspace(0,23.5,nMLT))
+            if kwargs.get('verbose',False):
+                print(f'\t\t\t{nMLT} MLTs')
+            mlt_lvls = np.linspace(0,23.5,nMLT)
             skip_mlt = True
         else:
             skip_mlt = False
@@ -56,27 +62,32 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
         while not done:
             lvl_line = [float(v) for v in f.readline().split()]
             unknown_lvls = np.concat([unknown_lvls,lvl_line])
-            if len(unknown_lvls)==nE:
+            if len(unknown_lvls)==nE and max(unknown_lvls)>90:
                 E_lvls = unknown_lvls
-                print(f'\t\t\t{nE} Energy Levels')
+                if kwargs.get('verbose',False):
+                    print(f'\t\t\t{nE} Energy Levels')
                 unknown_lvls = np.array([])
             elif len(unknown_lvls)==nMLT and not skip_mlt:
                 mlt_lvls = unknown_lvls
-                print(f'\t\t\t{nLat} MLT Levels')
+                if kwargs.get('verbose',False):
+                    print(f'\t\t\t{nLat} MLT Levels')
                 unknown_lvls = np.array([])
-            elif len(unknown_lvls)==nAlpha:
+            elif len(unknown_lvls)==nAlpha and max(unknown_lvls)<1:
                 alpha_lvls = unknown_lvls
-                print(f'\t\t\t{nAlpha} Pitch Angle Levels')
+                if kwargs.get('verbose',False):
+                    print(f'\t\t\t{nAlpha} Pitch Angle Levels')
                 unknown_lvls = np.array([])
-            elif len(unknown_lvls)==nLat:
+            elif len(unknown_lvls)==nLat and max(unknown_lvls)<90:
                 lat_lvls = unknown_lvls
-                print(f'\t\t\t{nLat} Latitude/Radial Levels')
+                if kwargs.get('verbose',False):
+                    print(f'\t\t\t{nLat} Latitude/Radial Levels')
                 unknown_lvls = np.array([])
             if all([E_lvls.any(),alpha_lvls.any(),
                     lat_lvls.any(),mlt_lvls.any()]):
                 done = True
             if i>(nLat+nE+nAlpha):
-                print(f'FAILED i={i}... check file')
+                if kwargs.get('verbose',False):
+                    print(f'FAILED i={i}... check file')
             i+=1
         data_start = f.tell()
         i_first_parmod = -999
@@ -87,10 +98,11 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
             if i==(i_first_parmod+1):
                 params2 = line.split()
             if ntimes==kwargs.get('maxtimes',999): break
-        print(f'\t\t\t{ntimes} Times')
+        if kwargs.get('verbose',False):
+            print(f'\t\t\t{ntimes} Times')
         times = np.zeros(ntimes)
         lstarMax = np.zeros(ntimes)
-        if len(params2)>6:
+        if len(params2)>9:
             #NOTE What I'm calling 'params2'
             # from cimi.f90 from cimipack_bb June2025, 10 values:
             #   xlati1(i,j)         - northern footpoint lat (deg)
@@ -98,11 +110,11 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
             #   xlatS1(i,j)         - southern footpoint lat (deg)
             #   xmltS(i,j)          - southern footpoint mlt (hours)
             #   ro1=ro(i,j)         - inner radius
-            #   xmlto1=xmlto(i,j)   - ???
-            #   BriN(i,j)
-            #   BriS(i,j)
-            #   bo(i,j)
-            #   ba(j)
+            #   xmlto1=xmlto(i,j)   - eq mapped mlt
+            #   BriN(i,j)           - North mirror point B
+            #   BriS(i,j)           - South mirror point B
+            #   bo(i,j)             - eq mapped B field
+            #   ba(j)               - index of last B field
             # i,j are lat and lon coordinates respectively
             # o denotes inner/near planet ie Bo=earths surface field
             flux_dict['latN'] = np.zeros([ntimes,nLat,nMLT])
@@ -144,18 +156,31 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
             flux_dict['Lstar']   = np.zeros([ntimes,nLat,nMLT])
             flux_dict['volume']  = np.zeros([ntimes,nLat,nMLT])
         else:
-            #TODO for the old case
-            pass
+            #NOTE 'params2' from the coupled version
+            # lat   - footpoint lat in N ionosphere
+            # mlt   - MLT in N ionosphere
+            # ro    - eq mapped radius
+            # mlto  - eq mapped MLT
+            # bo    - eq magnetic field strength
+            # irm   - index of latitude of last modeled field line
+            # iba   - ???
+            flux_dict['latN'] = np.zeros([ntimes,nLat,nMLT])
+            flux_dict['mltN'] = np.zeros([ntimes,nLat,nMLT])
+            flux_dict['ro']   = np.zeros([ntimes,nLat,nMLT])
+            flux_dict['mlto'] = np.zeros([ntimes,nLat,nMLT])
+            flux_dict['bo']   = np.zeros([ntimes,nLat,nMLT])
+            flux_dict['irm']  = np.zeros([ntimes,nLat,nMLT])
+            flux_dict['iba']  = np.zeros([ntimes,nLat,nMLT])
         f.seek(data_start)
         # Flux data
         flux = np.zeros([ntimes,nLat,nMLT,nE,nAlpha])
-        rcross = np.zeros([ntimes,nLat,nMLT])
         if mlt_lvls.max()>7:
             mltangles = np.array([(12-mlt)*np.pi/12 for mlt in mlt_lvls])
         else:
             mltangles = mlt_lvls # already in radians for mLon
         for itime in range(0,ntimes):
-            print(f'\t\tLoading time:{itime+1}/{ntimes}')
+            if kwargs.get('verbose',False):
+                print(f'\t\tLoading time:{itime+1}/{ntimes}')
             params1 = f.readline().split('!')[0].split()
             times[itime] = params1[0]
             lstarMax[itime]  = params1[1]
@@ -163,7 +188,7 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
             for ilat in range(0,nLat):
                 for imlt in range(0,nMLT):
                     params2 = f.readline().split()
-                    if len(params2)>6:
+                    if len(params2)>9:
                         flux_dict['latN'][itime,ilat,imlt] = params2[0]
                         flux_dict['mltN'][itime,imlt]      = params2[1]
                         flux_dict['latS'][itime,ilat,imlt] = params2[2]
@@ -189,10 +214,16 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
                         flux_dict['Lstar'][itime,ilat,imlt]   = params3[10]
                         flux_dict['volume'][itime,ilat,imlt]  = params3[11]
 
-                        rcross[itime,ilat,imlt] = params2[2]
                     else:
+                        flux_dict['latN'][itime,ilat,imlt] = params2[0]
+                        flux_dict['mltN'][itime,ilat,imlt] = params2[1]
+                        flux_dict['ro'][itime,ilat,imlt]   = params2[2]
+                        flux_dict['mlto'][itime,ilat,imlt] = params2[3]
+                        flux_dict['bo'][itime,ilat,imlt]   = params2[4]
+                        flux_dict['irm'][itime,ilat,imlt]  = params2[5]
+                        flux_dict['iba'][itime,ilat,imlt]  = params2[6]
+
                         params3 = None
-                        rcross[itime,ilat,imlt] = params2[2]
                     #if mlt_lvls.max()>7:
                     #    mlt_angle = (12-imlt)*np.pi/12
                     #else:
@@ -210,31 +241,37 @@ def read_flux(infile:str,**kwargs:dict) -> dict:
         flux_dict.update({'times':times,
                           'lat_lvls':lat_lvls,'mlt_lvls':mlt_lvls,
                           'E_lvls':E_lvls,'alpha_lvls':alpha_lvls,
-                          'rcross':rcross,'flux':flux})
+                          'flux':flux})
         return flux_dict
 
 def main() -> None:
     start_time = time.time()
     herepath=os.getcwd()
-    inpath = os.path.join('/Users/ambrenne/Code/SWMF/run/IM/plots/')
+    inpath = os.path.join('/Users/ambrenne/Code/run_mothersday_ne/RB/plots/')
     outpath = os.path.join(herepath,'gannon_rad_belt/analysis/')
     #inpath = os.path.join(herepath,'month_CIMI/')
     #outpath = os.path.join(herepath,'month_CIMI/')
-    filelist = glob.glob(f'{inpath}*_e.fls')
+    filelist = sorted(glob.glob(f'{inpath}*_e.fls'),key=time_sort)
     print(f'INPATH: {inpath}')
     #renderView = GetActiveViewOrCreate('RenderView')
     t0 = dt.datetime(1970,1,1)
-    for i,infile in enumerate(filelist[0:1]):
-        print(f"\t{i+1}/{len(filelist)}\t{infile.split('/')[-1]} ...")
+    flux_dict = {}
+    for i,infile in tqdm(enumerate(filelist)):
         fname = infile.split('/')[-1][:-6]
-        #tevent = dt.datetime.strptime(fname,'%Y%m%d_%H%M%S')
-        tevent = dt.datetime(2018,1,1,0)
+        tevent = dt.datetime.strptime(fname,'%Y%m%d_%H%M%S')
+        #tevent = dt.datetime(2024,5,10,13,0)
         ut = (tevent-t0).total_seconds()
         gp.recalc(ut)
-        flux_dict = read_flux(infile,maxtimes=999)
-        print(f"Converting {infile.split('/')[-1]} -> "+
-              f"{infile.replace('.fls','_fls.npz').split('/')[-1]}")
-        np.savez_compressed(infile.replace('.fls','_fls.npz'),**flux_dict)
+        flux = read_flux(infile,maxtimes=999,verbose=i==0)
+        if i==0:
+            print(f"\t{i+1}/{len(filelist)}\t{infile.split('/')[-1]} ...")
+            flux_dict = flux
+        for key in flux:
+            if len(flux[key].shape) > 2:
+                flux_dict[key] = np.concat([flux_dict[key],flux[key]])
+    print(f"Converting {infile.split('/')[-1]} -> "+
+          f"{infile.replace('.fls','_fls.npz').split('/')[-1]}")
+    np.savez_compressed(infile.replace('.fls','_fls.npz'),**flux_dict)
 
 if __name__ == "__main__":
     main()
